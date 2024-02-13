52fcbb0a385dae15a0e2162112399dd7d15668b4
atulit gaur <atulit23@gmail.com>
2024-02-13 17:56:44 +0530

    new

--- Main/index.mjs
+++ Main/index.mjs
@@ -7,6 +7,7 @@ const app = express();
 const port = 8000;
 
 const image_app = await client("https://atulit23-01.hf.space/");
+const flan_app = await client("https://atulit23-google-flan-t5.hf.space/");
 const image_app_1 = await client("https://atulit23-ui-deception.hf.space/");
 const review_app = await client("https://atulit23-deceptive-rev.hf.space/");
 
@@ -24,6 +25,12 @@ app.get("/review-prediction", async (req, res) => {
   const result = await review_app.predict("/predict", [url.url]);
   console.log(JSON.parse(result.data[0]))
   res.json(JSON.parse(result.data[0]));
+});
+
+app.get("/text-interference", async (req, res) => {
+  const text = req.query;
+  const result = await flan_app.predict("/predict", [text.text]);
+  res.send(result.data[0]);
 });
 
 app.get("/", (req, res) => {
